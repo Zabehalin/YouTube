2dadde809331ac9cc05b946995854ced5a9a8af2
root <root@localhost.localdomain>
2021-01-14 17:48:19 +0200

    req

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.proxy import *
 import time
 
 print("start")
-
+myProxy = "192.109.165.41:80"
+proxy = Proxy({'proxyType': ProxyType.MANUAL,'httpProxy': myProxy,'ftpProxy': myProxy, 'sslProxy': myProxy,'noProxy':''})
 options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
+options.headless = False
+driver = webdriver.Firefox(options=options, proxy=proxy)
 driver.get("https://www.youtube.com/watch?v=UIwhN3hHg7A")
 element = driver.find_element_by_class_name("ytp-play-button")
 element.click()
+time.sleep(15000)
